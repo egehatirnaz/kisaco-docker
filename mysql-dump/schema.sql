0aa9763a3220ca362bf297a90ed6f98b460d302a
egehatirnaz <egehatirnaz@hotmail.com>
2020-04-16 14:07:24 +0300

    Prometheus, grafana, alertmanager, etc. are added and connected to Docker.

--- mysql-dump/schema.sql
+++ mysql-dump/schema.sql
@@ -6,7 +6,7 @@ ALTER DATABASE
     COLLATE = utf8mb4_unicode_ci;
 USE kisaco_db;
 
-CREATE TABLE `User` (
+CREATE TABLE `ssers` (
 	`id` INT(10) PRIMARY KEY AUTO_INCREMENT,
 	`email` varchar(255) NOT NULL UNIQUE,
 	`password` varchar(60) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `User` (
 	`created_at` INT(11)
 );
 
-CREATE TABLE `URL` (
+CREATE TABLE `urls` (
 	`id` INT(10) PRIMARY KEY AUTO_INCREMENT,
 	`user_id` INT(10) NOT NULL,
 	`orig_url` VARCHAR(2000) NOT NULL,
@@ -25,19 +25,19 @@ CREATE TABLE `URL` (
 	`visitor_count` VARCHAR(10) NOT NULL DEFAULT '0',
 	`visitor_limit` VARCHAR(10) NOT NULL DEFAULT '0',
 	`is_private` BOOLEAN NOT NULL DEFAULT '0',
-	FOREIGN KEY (user_id) REFERENCES User(id) on delete cascade on update cascade
+	FOREIGN KEY (user_id) REFERENCES users(id) on delete cascade on update cascade
 );
 
-CREATE TABLE `URL_Request` (
+CREATE TABLE `url_requests` (
 	`id` INT(10) PRIMARY KEY AUTO_INCREMENT,
 	`url_id` INT(10) NOT NULL,
 	`request_ip` VARCHAR(41) NOT NULL,
 	`country_code` VARCHAR(2),
 	`request_referrer` VARCHAR(255),
 	`created_at` INT(11) NOT NULL,
-	FOREIGN KEY (url_id) REFERENCES URL(id) on delete cascade on update cascade
+	FOREIGN KEY (url_id) REFERENCES urls(id) on delete cascade on update cascade
 );
 
--- ALTER TABLE `URL` ADD CONSTRAINT `URL_fk0` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`);
+-- ALTER TABLE `urls` ADD CONSTRAINT `urls_fk0` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`);
 
--- ALTER TABLE `URL_Request` ADD CONSTRAINT `URL_Request_fk0` FOREIGN KEY (`url_id`) REFERENCES `URL`(`id`);
+-- ALTER TABLE `url_requests` ADD CONSTRAINT `url_requests_fk0` FOREIGN KEY (`url_id`) REFERENCES `urls`(`id`);
